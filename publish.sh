fde23c0770e2f7b5bb7b1bea44d9f4dabda1e9a8
Bastian Bukatz <bastian.greuel@gmail.com>
2020-11-04 20:26:55 +0100

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,2 +1,4 @@
-mvn deploy:deploy-file   -DgroupId=ch.einfachIT   -DartifactId=raspios_arm64   -Dversion=2020-08-24   -Dpackaging=zip   -Dfile=raspios_arm64.zip   -Dregistry=https://maven.pkg.github.com/einfachIT -Durl=https://maven.pkg.github.com/orgs/einfachIT/packages -Dtoken=${{secrets.GITHUB_TOKEN}}
+zip raspios_arm64.zip boot.tar.xz root.tar.xz os.json partitions.json partition_setup.sh
+
+deploy:deploy-file   -DgroupId=ch.einfachIT   -DartifactId=raspios_arm64   -Dversion=2020-08-24   -Dpackaging=zip   -Dfile=raspios_arm64.zip   -Dregistry=https://maven.pkg.github.com/einfachIT -Durl=https://maven.pkg.github.com/orgs/einfachIT/packages -Dtoken=${{secrets.GITHUB_TOKEN}}
 
