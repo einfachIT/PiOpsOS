d250251c272e4a01cb1472b0d8bfa132a57698a5
Bastian Bukatz <bastian.greuel@gmail.com>
2020-11-09 09:00:09 +0100

    moving OS specific scripts into Zip artefact

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# delete all artifacts
+rm raspios_arm64.zip boot.tar.xz root.tar.xz os.json partitions.json partition_setup.sh provision.service provision.sh blink_ip.sh blink_ip.service blink_ip.timer factory_reset.sh
