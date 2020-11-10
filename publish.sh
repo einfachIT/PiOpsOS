5fea117ca1fdd19d36faf5ac012013c85fe6ab95
Bastian Bukatz <bastian.greuel@gmail.com>
2020-11-10 08:29:58 +0100

    fixed artefact handling to be able publishing in the publish job within the build_publish workflow

--- publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# publish OS zip to gitHub packages maven repo
-deploy:deploy-file   -DgroupId=ch.einfachIT   -DartifactId=epicpios_64 -Dversion=2020-08-24   -Dpackaging=zip   -Dfile=epicPiOs_64.zip   -Dregistry=https://maven.pkg.github.com/einfachIT -Durl=https://maven.pkg.github.com/orgs/einfachIT/ -Dtoken=${{secrets.GITHUB_TOKEN}}
-
