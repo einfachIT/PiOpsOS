9f2e1370334ce7b4c6668af6e95226a0c7e45a4e
Bastian Bukatz <bastian.greuel@gmail.com>
2020-11-12 01:03:10 +0100

    added default wpa_supplicant

--- build.sh
+++ build.sh
@@ -10,6 +10,17 @@ cd noobs
 unzip ../noobs.zip
 sed -i 's/$/ silentinstall/' recovery.cmdline
 mkdir os/raspios_arm64
+cat >wpa_supplicant.conf <<EOF
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=CH
+
+network={
+        ssid="scratch"
+        psk="8962scratch"
+        key_mgmt=WPA_PSK
+}
+EOF
 cd ..
 
 # Download and prepare raspios beta image from official raspberry download page
@@ -168,3 +179,15 @@ for file in "boot.tar.xz" "os.json" "partitions.json" "partition_setup.sh" "root
 do
   cp $file noobs/os/raspios_arm64
 done
+
+cat >/noobs/wpa_supplicant.conf <<EOF
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=CH
+
+network={
+        ssid="scratch"
+        psk="8962scratch"
+        key_mgmt=WPA_PSK
+}
+EOF
