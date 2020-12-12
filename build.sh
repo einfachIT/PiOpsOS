2beb6bd9ca0e6e85abce1e623c936aadfbabc50b
Bastian Bukatz <bastian.greuel@gmail.com>
2020-12-12 13:07:37 +0100

    fixed ble service to autostart

--- build.sh
+++ build.sh
@@ -53,14 +53,19 @@ cd PI_ROOT/
   sudo ln -s /lib/systemd/system/provision.timer etc/systemd/system/timers.target.wants/provision.timer
   sudo ln -s /lib/systemd/system/update.timer etc/systemd/system/timers.target.wants/update.timer
   sudo chmod 0755 sbin/provision.sh 
+ 
   sudo cp ../blink_ip.sh sbin/blink_ip.sh
   sudo cp ../blink_ip.service lib/systemd/system/blink_ip.service
   sudo cp ../blink_ip.timer lib/systemd/system/blink_ip.timer
+  sudo ln -s /lib/systemd/system/blink_ip.timer etc/systemd/system/timers.target.wants/blink_ip.timer
+  sudo chmod 0755 sbin/blink_ip.sh
+ 
   sudo cp ../ble_ip.sh sbin/ble_ip.sh
   sudo cp ../ble_ip.service lib/systemd/system/ble_ip.service
   sudo cp ../ble_ip.timer lib/systemd/system/ble_ip.timer
-  sudo ln -s /lib/systemd/system/blink_ip.timer etc/systemd/system/timers.target.wants/blink_ip.timer
-  sudo chmod 0755 sbin/blink_ip.sh
+  sudo ln -s /lib/systemd/system/ble_ip.timer etc/systemd/system/timers.target.wants/ble_ip.timer
+  sudo chmod 0755 sbin/ble_ip.sh
+   
   sudo cp ../factory_reset.sh sbin/factory_reset.sh
   sudo chmod 0755 sbin/factory_reset.sh
   sudo bsdtar --numeric-owner --format gnutar --one-file-system -cpf ../root.tar .
